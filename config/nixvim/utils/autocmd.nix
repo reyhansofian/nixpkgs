be53216175084a0a305d118a85857458143a255b
Reyhan Sofian Haqqi <computeoholic@gmail.com>
2024-01-17 17:03:10 +0700

    fix: auto close nixvim when use close buffer

--- config/nixvim/utils/autocmd.nix
+++ config/nixvim/utils/autocmd.nix
@@ -211,40 +211,40 @@
           '';
         };
       }
-      {
-        event = [ "BufEnter" ];
-        group = "auto_quit";
-        desc = "Quit NixVim if more than one window is open and only sidebar windows are list";
-        callback = {
-          __raw = ''
-            function()
-              local wins = vim.api.nvim_tabpage_list_wins(0)
-              -- Both neo-tree and aerial will auto-quit if there is only a single window left
-              if #wins <= 1 then return end
-              local sidebar_fts = { aerial = true, ["neo-tree"] = true }
-              for _, winid in ipairs(wins) do
-                if vim.api.nvim_win_is_valid(winid) then
-                  local bufnr = vim.api.nvim_win_get_buf(winid)
-                  local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-                  -- If any visible windows are not sidebars, early return
-                  if not sidebar_fts[filetype] then
-                    return
-                  -- If the visible window is a sidebar
-                  else
-                    -- only count filetypes once, so remove a found sidebar from the detection
-                    sidebar_fts[filetype] = nil
-                  end
-                end
-              end
-              if #vim.api.nvim_list_tabpages() > 1 then
-                vim.cmd.tabclose()
-              else
-                vim.cmd.qall()
-              end
-            end
-          '';
-        };
-      }
+      # {
+      #   event = [ "BufEnter" ];
+      #   group = "auto_quit";
+      #   desc = "Quit NixVim if more than one window is open and only sidebar windows are list";
+      #   callback = {
+      #     __raw = ''
+      #       function()
+      #         local wins = vim.api.nvim_tabpage_list_wins(0)
+      #         -- Both neo-tree and aerial will auto-quit if there is only a single window left
+      #         if #wins <= 1 then return end
+      #         local sidebar_fts = { aerial = true, ["neo-tree"] = true }
+      #         for _, winid in ipairs(wins) do
+      #           if vim.api.nvim_win_is_valid(winid) then
+      #             local bufnr = vim.api.nvim_win_get_buf(winid)
+      #             local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+      #             -- If any visible windows are not sidebars, early return
+      #             if not sidebar_fts[filetype] then
+      #               return
+      #             -- If the visible window is a sidebar
+      #             else
+      #               -- only count filetypes once, so remove a found sidebar from the detection
+      #               sidebar_fts[filetype] = nil
+      #             end
+      #           end
+      #         end
+      #         if #vim.api.nvim_list_tabpages() > 1 then
+      #           vim.cmd.tabclose()
+      #         else
+      #           vim.cmd.qall()
+      #         end
+      #       end
+      #     '';
+      #   };
+      # }
       {
         event = [ "WinScrolled" ];
         group = "indent_blankline_refresh_scroll";
